9eb5beb8d274da832c402ed72ed5317f787dd7ab
Jafir <jianfei.han@fenrir-inc.com.cn>
2018-01-15 20:28:05 +0800

    Initial commit

---
+++ app/src/main/java/com/jafir/finishandstarttest/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.jafir.finishandstarttest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jafir on 2018/1/15.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+
+                /**
+                 * please change the order of finish and startActivity to test
+                 *
+                 * intent to Main
+                 * then click back key to close APP
+                 * open the recent task by clicking the switch key of phone
+                 * choose the APP
+                 * it will open Main directly
+                 * instead of Splash
+                 *
+                 */
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }.sendEmptyMessageDelayed(1, 3000);
+    }
+}
